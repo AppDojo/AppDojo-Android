d927485a2a294ca09f1a29787a09b628dd881ae8
Gregory Jean-Baptiste <gjean011@fiu.edu>
2013-07-21 21:12:15 -0400

    Adds login + Updates UI with logo

--- src/com/example/myfirstapp/MainActivity.java
+++ src/com/example/myfirstapp/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.myfirstapp;
 
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.app.Activity;
 import android.content.Intent;
@@ -25,25 +31,66 @@ client.get("https://appdojo-api.herokuapp.com/users", new AsyncHttpResponseHandl
     }
 });*/
 
-public class MainActivity extends Activity implements OnClickListener 
+public class MainActivity extends Activity 
 {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) 
     {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
-    
-        Button btnLogin = (Button) findViewById(R.id.buttonLogin);
-       
-        btnLogin.setOnClickListener(this);
     }
     
     
-    public void onClick(View w) 
+    /*public void onClick(View w) 
     {
     	Intent act2 = new Intent(this, screen2.class);
     	startActivity(act2);
+    }*/
+    
+    public void login(View log)
+    {
+    	EditText email_field = (EditText) findViewById(R.id.editEmail);
+    	String email = email_field.getText().toString();
+    	EditText pass_field = (EditText) findViewById(R.id.editPassword);
+    	String password = pass_field.getText().toString();
+    	
+    	RequestParams params = new RequestParams();
+    	params.put("email", email);
+    	params.put("password", password);
+    	
+    	Requests.post("api/v1/users/sign_in", params, new AsyncHttpResponseHandler() {
+    	    @Override
+    	    public void onSuccess(String response) 
+    	    {
+    	    	JSONObject json;
+    	    	JSONObject user;
+    	    	
+    	    	try 
+    	    	{
+					json = new JSONObject(response);
+					user = json.getJSONObject("user");
+					Log.w("test", user.getString("last_name"));
+				} 
+    	    	catch (JSONException jsone) 
+    	    	{
+					Log.w("test", "unable to convert string to json");
+					return;
+				}
+    	    }
+    	    
+    	    @Override
+    	    public void onFailure(Throwable e) 
+    	    {
+    	        Log.w("test", "OnFailure!", e);
+    	    }
+    	});
+    }
+    
+    public void register(View reg)
+    {
+    	
     }
     
 }
